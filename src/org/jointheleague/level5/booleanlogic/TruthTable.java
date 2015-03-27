52bd0f4382b64ac906c4c71bb88057c48d11063f
Stephen Cerruti <scerruti@otabi.com>
2015-03-26 20:35:11 -0700

    Sample solution for first coding problem.

--- src/org/jointheleague/level5/booleanlogic/TruthTable.java
+++ src/org/jointheleague/level5/booleanlogic/TruthTable.java
@@ -2,7 +2,17 @@ package org.jointheleague.level5.booleanlogic;
 
 public class TruthTable {
 	public String generateTable(TwoVariableFunction fn) {
-		return null;
+		StringBuilder response = new StringBuilder();
+		response.append("   A   |   B   | Result\n");
+		response.append("-------+-------+--------\n");
+		
+		for (boolean a : new boolean[] {true, false}) {
+			for (boolean b : new boolean[] {true, false}) {
+				response.append(String.format(" %5.5s | %5.5s | %s\n", a, b, fn.execute(a, b)));
+			}
+		}
+		
+		return response.toString();
 	}
 
 }
